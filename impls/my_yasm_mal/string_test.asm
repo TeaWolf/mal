0d5eeaddbee066a0ec5d0773a88b9697742ecf4e
Nicholas Greensmith <greensmith.nicholas@protonmail.com>
2020-04-22 15:01:50 +0200

    Adds string test file

--- impls/my_yasm_mal/string_test.asm
+++ impls/my_yasm_mal/string_test.asm
@@ -6,23 +6,40 @@ section .data
 
         EXIT_SUCCESS equ 0
 
-        spacer  db " : "
+        spacer  db " : ", NULL
         
         string1 db "Hey there here's a sentence", NULL
         string2 db "fuck", NULL
         string3 db "Here comes the Corona", NULL
+
+section .bss
+
+        stringBuffer resb 50
         
 section .text
 
 extern printString
 extern printNewLine
 extern stringLength
-
+extern int2string
+        
 %macro checkString 1
         lea rdi, [%1]
         call stringLength
         mov rbx, rax
+        
         call printString
+
+        lea rdi, byte [spacer]
+        call printString
+        
+        lea rsi, byte [stringBuffer]
+        mov rdi, rbx
+        call int2string
+
+        mov rdi, rsi
+        call printString
+        
         call printNewLine
 %endmacro
         
